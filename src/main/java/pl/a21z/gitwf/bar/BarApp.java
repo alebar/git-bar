27cad0b5f2ff677a4ef5e211ba9feb49f4f40a8f
Aleksander Bartnikiewicz <aleksander.bartnikiewicz@kruschecompany.com>
2016-11-27 22:33:24 +0100

    added dependency to the core lib

--- src/main/java/pl/a21z/gitwf/bar/BarApp.java
+++ src/main/java/pl/a21z/gitwf/bar/BarApp.java
@@ -2,10 +2,12 @@ package pl.a21z.gitwf.bar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import pl.a21z.gitwf.core.HelloWorldGenerator;
 
 @SpringBootApplication
 public class BarApp {
   public static void main(String[] args) {
+    System.out.println(new HelloWorldGenerator().generate());
     new SpringApplication(BarApp.class).run(args);
   }
 }
